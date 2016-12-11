f78e37d66cabb420f10d32f9a4ac384f12e73049
Michael Kortstiege <mk@nodexo.de>
2016-12-11 10:30:11 +0100

    fix bugs and slight refactoring

--- index.js
+++ index.js
@@ -19,53 +19,45 @@ class MOML {
     }
 
     // Fix multi-lines
-    try {
-      s = s.replace(/^([^:\s]+:)([ \t]+)(.*\n)([ \t]+)([^\s]+)/gm,
-        function (match, $1, $2, $3, $4, $5, offset, original) {
-          return $1 + ' |\n' + $4 + $3 + $4 + $5
-        })
-    } catch (e) {
-      console.log(e.message)
-    }
+    s = s.replace(/^([^:\s]+:)([ \t]+)(.*\n)([ \t]+)([^\s]+)/gm,
+      function (match, $1, $2, $3, $4, $5, offset, original) {
+        return $1 + ' |\n' + $4 + $3 + $4 + $5
+      })
 
     // Parse YAML
     let data
-    try {
-      data = yaml.safeLoad(s)
-    } catch (e) {
-      console.log('HERE:' + e.message)
+    data = yaml.safeLoad(s)
+
+    if (Object.prototype.toString.call(data) !== '[object Object]') {
+      throw new Error('Malformed input data: ' + s)
     }
 
     // Transform data
     let result = {}
-    try {
-      for (let k of Object.keys(data)) {
-        let key = k.toLowerCase()
-        let value = data[k]
-        if (value) {
-          if (typeof value === 'string') {
-            value = value
-              .replace(/\n+$/, '')
-              .replace(/_[ \r\t]*\n/g, '')
-          }
-
-          if (key.match(/\[\]$/)) {
-            key = key.substring(0, key.length - 2)
-            if (value.match(/\n/)) {
-              value = value.split(/\n/g)
-            } else {
-              value = value.split(/,/g)
-            }
-            for (let i in value) {
-              value[i] = value[i].trim()
-            }
-          }
-
-          result[key] = value
+    for (let k of Object.keys(data)) {
+      let key = k.toLowerCase()
+      let value = data[k]
+      if (value) {
+        if (typeof value === 'string') {
+          value = value
+            .replace(/\n+$/, '')
+            .replace(/_[ \r\t]*\n/g, '')
         }
+
+        if (key.match(/\[\]$/)) {
+          key = key.substring(0, key.length - 2)
+          if (value.match(/\n/)) {
+            value = value.split(/\n/g)
+          } else {
+            value = value.split(/,/g)
+          }
+          for (let i in value) {
+            value[i] = value[i].trim()
+          }
+        }
+
+        result[key] = value
       }
-    } catch (e) {
-      console.log(e.message)
     }
 
     return result
